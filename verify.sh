a269276c7aaa7fe5053fccfe954dc27abcee7d19
Dhi Aurrahman <dio@rockybars.com>
2021-09-14 06:57:40 -0700

    TEst
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- verify.sh
+++ verify.sh
@@ -8,6 +8,6 @@ ARCH=${ARCH:-amd64}
 declare -A install=(["deb"]="dpkg -i" ["rpm"]="rpm -i")
 declare -A uninstall=(["deb"]="apt remove -yqq" ["rpm"]="rpm -e")
 
-${install[${PACKAGING}]} func-e_dev_linux_${ARCH}.${PACKAGING}
+${install[${PACKAGING}]} dist/func-e_dev_linux_${ARCH}.${PACKAGING}
 func-e versions
 ${uninstall[${PACKAGING}]} func-e
