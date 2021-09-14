53c536180b5dbcd99e375fcac296b8eaf2366cf0
Dhi Aurrahman <dio@rockybars.com>
2021-09-14 07:11:37 -0700

    Fix
    Signed-off-by: Dhi Aurrahman <dio@rockybars.com>

--- verify.sh
+++ verify.sh
@@ -4,11 +4,11 @@ set -euo pipefail
 
 PACKAGING=${PACKAGING:-deb}
 ARCH=${ARCH:-amd64}
-PKG_FILE=${PKG_FILE:-dist/func-e_dev_linux_${ARCH}.${PACKAGING}}
+FILE=${FILE:-dist/func-e_dev_linux_${ARCH}.${PACKAGING}}
 
 declare -A install=(["deb"]="dpkg -i" ["rpm"]="rpm -i")
 declare -A uninstall=(["deb"]="apt remove -yqq" ["rpm"]="rpm -e")
 
-${install[${PACKAGING}]} ${PKG_FILE}
+${install[${PACKAGING}]} ${FILE}
 func-e versions
 ${uninstall[${PACKAGING}]} func-e
